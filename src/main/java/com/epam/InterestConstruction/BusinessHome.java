abd6d0c4159378a58866f11ab10f8c8974cf7a9e
Saaiheswar <saaiheswar@gmail.com>
2020-02-20 21:46:08 +0530

    This is 1st  commit

---
+++ src/main/java/com/epam/InterestConstruction/BusinessHome.java
@@ -0,0 +1,8 @@
+package com.epam.InterestConstruction;
+
+public class BusinessHome extends ClientApplication{
+
+    public int construction_cost(int n,int area) {
+        return (n*area);
+    }
+}
